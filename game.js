ac6aa1bd0a354f03c4015c99ea5f2af36b1e3495
junfengl95 <junfeng011995@gmail.com>
2024-07-16 17:31:29 +0800

    Add overlay to screen

--- game.js
+++ game.js
@@ -1,11 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    function createPlayer(sign, name, id){
+        let score = 0;
+
+        // method to update score
+        const updateScore = () => {
+            score++;
+        }
+
+        const getScore = () => score; // Implicit return
+
+        const getSign = () => sign;
+
+        const getName = () => name;
+
+        const getId = () => id;
+
+        return {getSign, getName, getId, updateScore, getScore}
+    }
+
     const Gameboard = {
         board: ["", "", "", "", "", "", "", "", ""],
         resetBoard() {
             this.board = ["", "", "", "", "", "", "", "", ""];
         },
     };
+
+
+    // Dialog and form handling
+    const dialog = document.getElementById('game-dialog');
+    const form = document.getElementById('game-form')
 
     const buttons = document.querySelectorAll(".empty-cell");
 
@@ -15,13 +39,22 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
-    const Player = (name, marker) => {
-        return { name, marker };
-    };
 
-    const Game = {
-        player1: Player("Player 1", "X"),
-        player2: Player("Player 2", "O"),
+    // Overlay 
+    const closeOverlay = () => {
+        document.getElementById('overlay').style.display = 'none';
+    }
+
+    const openOverlay = () => {
+        document.getElementById('overlay').style.diplay = 'block';
+    }
+
+    const submitBtn = document.getElementById("submit-button");
+
+    submitBtn.onclick = function(event){
+        event.preventDefault();
+
+        closeOverlay();
     }
 
 })
